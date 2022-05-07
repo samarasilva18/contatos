0243bfaf10309682a99a474227e7e95e5f6d786c
SAMARA SILVA <1090481921021@fatec-srv.local>
2022-05-07 12:05:50 -0300

    Version 1.0

--- src/main/java/com/samara/contatos/repository/Contatos.java
+++ src/main/java/com/samara/contatos/repository/Contatos.java
@@ -4,6 +4,5 @@ import com.samara.contatos.model.Contato;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Contatos extends JpaRepository<Contato, Long>{
-    
+public interface Contatos extends JpaRepository<Contato, Long> {
 }
